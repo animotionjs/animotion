59913677b6f75802c889314be1ef484f3679b4d6
mattcroat <mattcroat.dev@gmail.com>
2023-06-22 19:15:47 +0200

    feat: Step component

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,8 @@
 import Presentation from './presentation.svelte'
 import Slide from './slide.svelte'
 import Code from './code.svelte'
+import Markdown from './markdown.svelte'
+import Media from './media.svelte'
+import Step from './step.svelte'
 
-export { Presentation, Slide, Code }
+export { Presentation, Slide, Code, Markdown, Media, Step }
