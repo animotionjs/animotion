df9516a60446f4dd3f5c6c5a72ec9d0fad4099c9
mattcroat <mattcroat.dev@gmail.com>
2023-06-22 17:07:21 +0200

    feat: Markdown component

---
+++ src/lib/components/markdown.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let file: string
+</script>
+
+{#if file}
+	<section data-markdown={file} />
+{:else}
+	<section data-markdown>
+		<div data-template>
+			<slot />
+		</div>
+	</section>
+{/if}
