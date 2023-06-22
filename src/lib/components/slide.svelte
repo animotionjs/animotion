a4be91d45b3d6f574475b397894ff0a48243b8a9
mattcroat <mattcroat.dev@gmail.com>
2023-06-22 19:15:03 +0200

    feat: Slides data attributes

--- src/lib/components/slide.svelte
+++ src/lib/components/slide.svelte
@@ -9,6 +9,12 @@
 	export let animate = false
 	export let animateId = null
 	export let animateRestart = null
+	export let background = null
+	export let gradient = null
+	export let image = null
+	export let video = null
+	export let iframe = null
+	export let interactive = null
 
 	const slideId = id++
 	const dispatch = createEventDispatcher()
@@ -21,6 +27,12 @@
 	data-auto-animate={animate || null}
 	data-auto-animate-id={animateId}
 	data-auto-animate-restart={animateRestart}
+	data-background-color={background}
+	data-background-gradient={gradient}
+	data-background-image={image}
+	data-background-video={video}
+	data-background-iframe={iframe}
+	data-background-interactive={interactive}
 	{...$$props}
 >
 	<slot />
