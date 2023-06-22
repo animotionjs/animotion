ee8cda12f9706996ea5f9d304c937bfbd8923a2f
mattcroat <mattcroat.dev@gmail.com>
2023-06-22 17:07:38 +0200

    feat: Media component

---
+++ src/lib/components/media.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	type Element = 'video' | 'image' | 'iframe'
+
+	export let type: Element
+	export let src: string
+	export let autoplay = false
+	export let preload = false
+</script>
+
+<svelte:element
+	this={type}
+	data-src={src}
+	data-autoplay={autoplay || null}
+	data-prelod={preload || null}
+	{...$$props}
+>
+	<slot />
+</svelte:element>
