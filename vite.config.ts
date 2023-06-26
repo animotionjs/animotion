e874592c7ab8083f4b526df6ee30538945575188
mattcroat <mattcroat.dev@gmail.com>
2023-06-26 22:08:31 +0200

    chore: Update aliases

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,11 @@ export default defineConfig({
 	plugins: [svelte()],
 	resolve: {
 		alias: {
-			'@components': path.resolve(__dirname, './src/lib/components/index.ts'),
 			'@config': path.resolve(__dirname, './src/config.ts'),
+			'@deck': path.resolve(__dirname, './src/lib/components/index.ts'),
 			'@languages': path.resolve(__dirname, './src/lib/languages/index.ts'),
 			'@lib': path.resolve(__dirname, './src/lib'),
+			'@store': path.resolve(__dirname, './src/lib/store/index.ts'),
 			'@styles': path.resolve(__dirname, './src/lib/styles'),
 		},
 	},
