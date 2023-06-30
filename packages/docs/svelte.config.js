be757e52a9e894a49849283be6b71e34c39f9ae5
mattcroat <mattcroat.dev@gmail.com>
2023-06-30 16:33:04 +0200

    feat: Docs

---
+++ packages/docs/svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/kit/vite'
+
+const dev = process.env.NODE_ENV === 'dev'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/keynote'
+		}
+	}
+}
+
+export default config
