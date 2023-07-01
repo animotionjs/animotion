ddbd6cf9e411f419fc47db96bc113fa82fed3ac9
mattcroat <mattcroat.dev@gmail.com>
2023-07-01 21:36:59 +0200

    wip: Docs

--- packages/docs/src/routes/+page.svelte
+++ packages/docs/src/routes/+page.svelte
@@ -1,34 +1,52 @@
 <script lang="ts">
 	import { base } from '$app/paths'
+	import { fade } from '$lib/transitions/fade'
+
+	let videoEl: HTMLVideoElement
 </script>
 
-<main class="container center text-center">
-	<h1 class="heading text-center capitalize">
+<main class="container place-content-center">
+	<h1 use:fade class="heading margin-text-center capitalize">
 		Create beautiful animated presentations with ease
 	</h1>
 
-	<p class="subheading">
+	<p
+		use:fade={{ duration: 0.3, delay: 0.8 }}
+		on:finish={() => videoEl.play()}
+		class="subheading margin-text-center"
+	>
 		<a href="{base}/docs/getting-started">Visualize ideas</a> with code using web
 		technologies.
 	</p>
 
-	<video src="keynote.mp4" autoplay loop />
+	<div class="video">
+		<video bind:this={videoEl} src="video.mp4" muted loop />
+	</div>
 </main>
 
 <style lang="postcss">
 	.heading {
-		max-width: 800px;
-		font-size: var(--font-size-5);
+		max-width: 400px;
+		font-size: var(--font-size-4);
 		line-height: 1.4;
 		letter-spacing: 1.4px;
 		margin-block-start: var(--size-6);
+
+		@media (width > 800px) {
+			max-width: 800px;
+			font-size: var(--font-size-5);
+		}
 	}
 
 	.subheading {
 		max-width: 800px;
 		margin-block: var(--size-4);
-		font-size: var(--font-size-4);
-		color: var(--text-2-dark);
+		font-size: var(--font-size-3);
+		color: var(--text-2);
+
+		@media (width > 800px) {
+			font-size: var(--font-size-4);
+		}
 	}
 
 	.subheading {
@@ -36,19 +54,24 @@
 			font-weight: 600;
 			text-decoration-line: underline;
 			text-decoration-style: wavy;
-			text-decoration-color: aqua;
+			text-decoration-color: var(--brand);
 
 			&:hover {
-				color: var(--text-1);
+				color: var(--brand);
 			}
 		}
 	}
 
-	video {
+	.video {
 		max-width: 800px;
-		aspect-ratio: 16/9;
-		object-fit: cover;
-		border-radius: var(--radius-1);
-		box-shadow: var(--shadow-1);
+		perspective: 1000px;
+
+		video {
+			aspect-ratio: 16/9;
+			object-fit: cover;
+			transform: rotateX(40deg) translateY(-20%);
+			border-radius: var(--radius-1);
+			box-shadow: var(--shadow-1);
+		}
 	}
 </style>
---
+++ packages/docs/static/theme.js
@@ -0,0 +1,16 @@
+function setTheme() {
+	const setTheme = localStorage.getItem('theme')
+	const prefersColorScheme = window.matchMedia(
+		'(prefers-color-scheme: light)'
+	).matches
+	const theme = prefersColorScheme ? 'light' : 'dark'
+
+	if (setTheme) {
+		document.documentElement.setAttribute('color-scheme', setTheme)
+		return
+	}
+
+	document.documentElement.setAttribute('color-scheme', theme)
+}
+
+setTheme()
