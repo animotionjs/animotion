2e7234e54f78e9e6634ec11abffa9bda25a00cdf
mattcroat <mattcroat.dev@gmail.com>
2023-07-16 17:06:50 +0200

    docs: SEO

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # Keynote
 
-Create beautiful presentations with Svelte.
+<div align="center">
+ <img src="packages/docs/static/logo.png" alt="Keynote logo">
+</div>
+
+## What Is Keynote?
+
+Keynote is a presentational framework for creating beautiful slides and visualizing ideas with code.
+
+## Contributing
+
+Keynote is a monorepo using [pnpm workspaces](https://pnpm.io/workspaces) for easier organization broken down into these packages.
+
+| Package          | Description   |
+| ---------------- | ------------- |
+| `create-keynote` | Keynote CLI   |
+| `docs`           | Documentation |
+| `keynote`        | Keynote       |
+
+To set up the project first clone it.
+
+```sh
+git clone https://github.com/mattcroat/keynote
+```
+
+Install dependencies for every package.
+
+```sh
+pnpm i
+```
+
+If you want to make changes to the docs.
+
+```sh
+pnpm docs:dev
+```
+
+If you want to make changes to Keynote.
+
+```sh
+pnpm keynote:dev
+```
+
+You can also `cd` into the respective package directory and run `pnpm run dev`, or `pnpm run build` and `pnpm run preview` for the package.
--- packages/docs/src/routes/+layout.svelte
+++ packages/docs/src/routes/+layout.svelte
@@ -3,12 +3,34 @@
 	import Header from './header.svelte'
 	import '../app.postcss'
 
-	$: ({ title, content } = $page.data)
+	$: ({ title } = $page.data)
 </script>
 
 <svelte:head>
 	<title>{title ?? 'Keynote'}</title>
-	<meta name="description" {content} />
+
+	<meta
+		content="Keynote is a presentational framework for creating beautiful slides and visualizing ideas with code."
+		name="description"
+	/>
+
+	<meta content={title ?? 'Keynote'} property="og:title" />
+	<!-- <meta content={social} property="og:image" /> -->
+	<!-- <meta content="https://joyofcode.xyz/" property="og:url" /> -->
+	<meta
+		content="Keynote is a presentational framework for creating beautiful slides and visualizing ideas with code."
+		property="og:description"
+	/>
+	<meta content="Keynote" property="og:site_name" />
+
+	<meta content="@joyofcodedev" name="twitter:creator" />
+	<meta content="summary_large_image" name="twitter:card" />
+	<meta content={title ?? 'Keynote'} name="twitter:title" />
+	<meta
+		content="Keynote is a presentational framework for creating beautiful slides and visualizing ideas with code."
+		name="twitter:description"
+	/>
+	<!-- <meta content={social} name="twitter:image" /> -->
 </svelte:head>
 
 <Header />
