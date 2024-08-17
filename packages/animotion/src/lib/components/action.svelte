bb0239976245dbbe010c060ee80bc0701380e3b3
mattcroat <mattcroat.dev@gmail.com>
2024-08-17 15:08:16 +0200

    feat: add undo for actions

--- packages/animotion/src/lib/components/action.svelte
+++ packages/animotion/src/lib/components/action.svelte
@@ -2,19 +2,24 @@
 	type ActionProps = {
 		order?: number
 		do?: () => void
+		undo?: () => void
 	}
 
 	let { order, ...props }: ActionProps = $props()
 	let el: HTMLDivElement
 
-	function action() {
-		const noop = () => {}
-		props?.do?.() ?? noop()
-	}
+	const noop = () => {}
+	const action = () => props?.do?.() ?? noop()
+	const undo = () => props?.undo?.() ?? noop()
 
 	$effect(() => {
 		el.addEventListener('in', action)
-		return () => el.removeEventListener('in', action)
+		el.addEventListener('out', undo)
+
+		return () => {
+			el.removeEventListener('in', action)
+			el.removeEventListener('out', undo)
+		}
 	})
 </script>
 
