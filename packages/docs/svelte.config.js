e31b1939566c1811a8c21e2a4aa217d1ffd99d19
mattcroat <mattcroat.dev@gmail.com>
2025-05-18 20:52:46 +0200

    chore: remove GitHub pages deploy

--- packages/docs/svelte.config.js
+++ packages/docs/svelte.config.js
@@ -2,17 +2,12 @@ import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import sveltemark from './src/lib/sveltemark/index.js'
 
-const dev = process.env.NODE_ENV === 'development';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [sveltemark(), vitePreprocess()],
 	kit: {
 		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/animotion'
-		}
 	}
 }
 
