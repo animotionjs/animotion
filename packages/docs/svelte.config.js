5c8f427dde2985801a56de2213c673024033ea8c
mattcroat <mattcroat.dev@gmail.com>
2025-05-18 20:18:06 +0200

    feat: add GitHub Pages deploy

--- packages/docs/svelte.config.js
+++ packages/docs/svelte.config.js
@@ -2,12 +2,17 @@ import adapter from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import sveltemark from './src/lib/sveltemark/index.js'
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 	preprocess: [sveltemark(), vitePreprocess()],
 	kit: {
 		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/animotion'
+		}
 	}
 }
 
