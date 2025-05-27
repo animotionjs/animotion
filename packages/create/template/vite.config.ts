64869459ed0a72fc662ac0622b3a0e6836a34a40
mattcroat <mattcroat.dev@gmail.com>
2025-05-27 16:58:02 +0200

    fix: plugin order

--- packages/create/template/vite.config.ts
+++ packages/create/template/vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+	plugins: [tailwindcss(), sveltekit()],
 })
