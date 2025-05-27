d3bda75a5bb01d4fa1965c35da889c47802b03fb
mattcroat <mattcroat.dev@gmail.com>
2025-05-27 16:09:49 +0200

    chore: add file-based slides example

---
+++ packages/animotion/src/routes/file-based/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Presentation, Slides } from '$lib/index.js'
+</script>
+
+<Presentation options={{ history: true, transition: 'slide', controls: true, progress: true }}>
+	<Slides center={true} />
+</Presentation>
---
+++ packages/animotion/src/slides/100/slide.svelte
@@ -0,0 +1,1 @@
+<p class="text-4xl font-bold drop-shadow-sm">🪄 Use arrow keys to navigate</p>
