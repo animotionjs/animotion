81748c1fda01ba56b8d62c534422d0347af45673
matiadev <mattcroat.dev@gmail.com>
2026-02-08 18:52:46 +0100

    feat: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	test: {
+		browser: {
+			provider: playwright({
+				contextOptions: {
+					colorScheme: 'dark'
+				}
+			}),
+			enabled: true,
+			instances: [{ browser: 'chromium' }],
+			headless: true
+		}
+	}
 })
